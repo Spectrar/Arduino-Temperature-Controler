2ce607b9ede20b7bfed5641fbb362a5338b09adc
Brandon <35783078+Spectrar@users.noreply.github.com>
2018-01-25 14:38:14 -0700

    Add files via upload

---
+++ Arduino_temp_reader/Arduino_temp_reader.ino
@@ -0,0 +1,32 @@
+
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+// pin2
+#define ONE_WIRE_BUS 2 
+
+//OneWire reading all pins
+OneWire oneWire(ONE_WIRE_BUS); 
+
+// tell Dallas Temperature to read pins
+DallasTemperature sensors(&oneWire);
+
+void setup(void) 
+{ 
+ // start serial port 
+ Serial.begin(9600); 
+ // Start sensors 
+ sensors.begin(); 
+} 
+void loop(void) 
+{ 
+ //get temperature readings 
+ sensors.requestTemperatures();
+
+ Serial.print("Temperature is:   1: "); 
+ Serial.print(sensors.getTempFByIndex(0));
+ Serial.print("     2: ");
+ Serial.print(sensors.getTempFByIndex(1));
+ Serial.println();
+ delay(1000); 
+} 
